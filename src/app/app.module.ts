cb19898ce42aaad376102c52275b6707218487f3
samofoke <sabataernestmofokeng@gmail.com>
2024-03-06 17:29:20 +0200

    file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LeaderboardsComponent } from './pages/leaderboards/leaderboards.component';
+import { MessagesComponent } from './pages/messages/messages.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LeaderboardsComponent, MessagesComponent, ProfileComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
+  exports: [],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/players/players.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Player } from '../../interfaces/players';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-players',
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.css'],
+})
+export class PlayersComponent implements OnInit {
+  public players$: Observable<Player[] | undefined> = of(undefined);
+
+  constructor(private api: ApiService) {}
+
+  public ngOnInit(): void {
+    this.players$ = this.api.getAllPlayers$();
+  }
+
+  public update(text: string) {
+    this.players$ = this.api.getPlayersByName$(text);
+  }
+}
